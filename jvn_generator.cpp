371bbceceb5f96c78a3803b74c975af07494fac6
Radovan Wagner <radovan.wagner@solwer.sk>
2020-07-07 03:10:22 +0200

    Rozrobene - idem spat

--- jvn_generator.cpp
+++ jvn_generator.cpp
@@ -1,10 +1,21 @@
+#include <stdio.h>
+#include <string.h>
 #include "jvn_generator.hpp"
 
 long msm( unsigned int dlzka, unsigned int seed )
 {
-    long mocnina = 
+    char            mocnina[ MAX_DLZKA_CISLA*2+1 ];
+    char            seed[ MAX_DLZKA_CISLA+1 ];
+    char            cislo[ MAX_DLZKA_CISLA+1 ];
+    unsigned long   vysledok=0;
+    
     if( dlzka > MAX_DLZKA_CISLA )
         return( ERR_OVERFLOW );
+
+    sprintf( mocnina, "%ld", seed*seed  );
+    sscanf( "%d",      &vysledok );
+
+
 
     
     
--- test1.cpp
+++ test1.cpp
@@ -3,6 +3,13 @@
 
 int main( int argc, const char *argv[], const char *env[] )
 {
+     int             i=0;
+
+    while( i++ < 10 )
+    {
+        sprintf( cislo, "%ld", jvn_generator( 4, 1234 ) );
+
+    }
     printf( "MAX_DLZKA_CISLA = %d\n", MAX_DLZKA_CISLA );
     return( 0 );
 }
