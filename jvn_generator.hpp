ee6514b2184cdd9d1361948c95359430ec38a507
Radovan Wagner <radovan.wagner@solwer.sk>
2020-07-07 02:13:20 +0200

    bbb

--- jvn_generator.hpp
+++ jvn_generator.hpp
@@ -2,10 +2,9 @@
 #define __JVN_GENERATOR
 
     #include <math.h>
-    // #define MAX_DLZKA_CISLA (unsigned int)log10((double)(2^((sizeof(long)*8)-1.0)/2.0))
+    #define MAX_DLZKA_CISLA (unsigned int)log10((double)(  pow( 2.0, (double)(sizeof(long)*8-1.0))/2.0))
 
-    
-    #define MAX_DLZKA_CISLA (unsigned int)pow( 2.0, 10.0 )
+    // #define MAX_DLZKA_CISLA (unsigned int)pow( 2.0, 10.0 )
     #define DEFAULT_SEED 1234
     #define ERR_OVERFLOW -1
 
